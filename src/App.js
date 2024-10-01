e105225626d3142e9637f9c8ae6f34e5a2f9ee21
kondziux1 <konrad.frukacz@op.pl>
2024-10-01 17:45:35 +0200

    wykonanie aplikacji, formularz gotowy skrypt również

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
+  function onSubmit(event) {
+    event.preventDefault();
+
+    const title = event.target.elements.filmTitle.value;
+    const type = event.target.elements.filmType.value;
+    console.log(`tytul: ${title}, rodzaj: ${type}`);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={onSubmit} method="post">
+        <div className="form-group">
+          <label htmlFor="filmTitle">Tytuł filmu</label>
+          <input
+            type="text"
+            name="filmTitle"
+            id="filmTitle"
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="filmType">Rodzaj</label>
+          <select name="filmType" id="filmType" className="form-control">
+            <option></option>
+            <option value="1">Komedia</option>
+            <option value="2">Obyczajowy</option>
+            <option value="3">Sensacyjny</option>
+            <option value="4">Horror</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <button type="submit" className="btn btn-primary">
+            Dodaj
+          </button>
+        </div>
+      </form>
+    </>
   );
 }
 
